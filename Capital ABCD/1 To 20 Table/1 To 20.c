c30c212fae54d2179cb1ca827f14fb63060c36db
het2809 <hetk93@gmail.com>
2023-05-03 16:20:53 +0530

    i added 1 to 20 file

---
+++ Capital ABCD/1 To 20 Table/1 To 20.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("enter number");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("1*%d=%d\n", i, i * 1);
+        }
+        break;
+
+    case 2:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("2*%d=%d\n", i, i * 2);
+        }
+        break;
+
+    case 3:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("3*%d=%d\n", i, i * 3);
+        }
+        break;
+    case 4:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("4*%d=%d\n", i, i * 4);
+        }
+        break;
+
+    case 5:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("5*%d=%d\n", i, i * 5);
+        }
+        break;
+    case 6:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("6*%d=%d\n", i, i * 6);
+        }
+        break;
+    case 7:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("7*%d=%d\n", i, i * 7);
+        }
+        break;
+    case 8:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("8*%d=%d\n", i, i * 8);
+        }
+        break;
+    case 9:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("9*%d=%d\n", i, i * 9);
+        }
+        break;
+    case 10:
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            printf("10*%d=%d\n", i, i * 10);
+        }
+        break;
+    case 11:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("11*%d=%d\n", i, i * 11);
+        }
+        break;
+     case 12:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("12*%d=%d\n", i, i * 12);
+        }
+        break;
+     case 13:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("13*%d=%d\n", i, i * 13);
+        }
+        break;
+     case 14:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("14*%d=%d\n", i, i * 14);
+        }
+        break;
+     case 15:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("15*%d=%d\n", i, i * 15);
+        }
+        break;
+     case 16:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("16*%d=%d\n", i, i * 16);
+        }
+        break;
+     case 17:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("17*%d=%d\n", i, i * 17);
+        }
+        break;
+     case 18:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("18*%d=%d\n", i, i * 18);
+        }
+        break;
+     case 19:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("19*%d=%d\n", i, i * 19);
+        }
+        break;
+     case 20:
+
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("20*%d=%d\n", i, i * 20);
+        }
+        break;
+
+
+    default:
+    {
+        printf("wait....we will update soon");
+    }
+        break;
+    }
+}
