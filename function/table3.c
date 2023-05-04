fdde0d37b49ee21655d0c5e902ff614fdf997ce5
het2809 <132273406+het2809@users.noreply.github.com>
2023-05-04 17:07:10 +0530

    added space

--- function/table3.c
+++ function/table3.c
@@ -5,8 +5,9 @@ int main()
     int n;
     printf("enter number");
     scanf("%d", &n);
+    
     for (int i = 1; i <= 10; i++)
     {
-        printf("%d*%d=%d\n", n, i, i * n);
+        printf("%d * %d = %d\n", n, i, i * n);
     }
 }
