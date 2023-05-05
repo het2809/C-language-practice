00c15e517b2caace2f1a5d2f6ce4f0901b28846a
het2809 <hetk93@gmail.com>
2023-05-05 09:40:57 +0530

    Added Circle Rectangle And Square Area Calculation program

---
+++ function/circle-rectangle-squarearea.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+float SquareArea(float side);
+float CirceAre(float rad);
+float RectangleArea(float a, float b);
+
+int main()
+{
+    float a;
+    float b;
+    float side;
+    float rad;
+
+    printf("enter your a for ractangle area");
+    scanf("%f", &a);
+    printf("enter your b for ractangle area");
+    scanf("%f", &b);
+    printf("The Value of rectangle area is %f\n", RectangleArea(a, b));
+
+    printf("enter your side for square area");
+    scanf("%f", &side);
+    printf("The Value of square area is %f\n", SquareArea(side));
+
+    printf("enter your rad for Circle area");
+    scanf("%f", &rad);
+    printf("enter your rad for Circle area");
+    scanf("%f", &rad);
+    printf("The Value Of Circle Area is %f",CirceAre(rad));
+}
+
+float SquareArea(float side)
+{
+    return side * side;
+}
+
+float CirceAre(float rad)
+{
+    return 3.14 * rad * rad;
+}
+float RectangleArea(float a, float b)
+{
+    return a * b;
+}
---
+++ function/square.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main()
+{
+    int a;
+
+    printf("enter number");
+    scanf("%d",&a);
+    printf("%f",pow (a,2));
+
+    
+}
